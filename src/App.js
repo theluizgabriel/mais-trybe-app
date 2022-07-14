1190daf89b1517bb5649787c9dbbe2564df70919
PauloMisaki <ph.sorata@gmail.com>
2022-07-14 16:40:14 -0300

    fix: app conflict

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './components/Login';
+import './App.css';
+import rockGlass from './images/rockGlass.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import GlobalProvider from './context/contextProvider';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={ Login } />
-    </Switch>
+    <GlobalProvider>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+      </Switch>
+      <div className="meals">
+        <span className="logo">TRYBE</span>
+        <object
+          className="rocksGlass"
+          type="image/svg+xml"
+          data={ rockGlass }
+        >
+          Glass
+        </object>
+      </div>
+    </GlobalProvider>
+
   );
 }
 
