a3a296a6fb7a14b04a864ec4c571bf10d5c39f0c
Podence <podence23@gmail.com>
2022-07-22 14:20:32 -0300

    merge

--- src/pages/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 import Filters from '../components/Filters';
